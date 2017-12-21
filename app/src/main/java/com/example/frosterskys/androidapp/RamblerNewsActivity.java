850d81bbef0e3dd7853f70e6d57bd383deadfd05
evbe0615 <evgenii.belobragin@netcracker.com>
2017-12-21 17:17:50 +0300

    dfsdfsfsdfsd

---
+++ app/src/main/java/com/example/frosterskys/androidapp/RamblerNewsActivity.java
@@ -0,0 +1,10 @@
+package com.example.frosterskys.androidapp;
+
+import android.app.Activity;
+
+/**
+ * Created by evbe0615 on 21-Dec-17.
+ */
+
+public class RamblerNewsActivity extends Activity {
+}
--- app/src/main/java/com/example/frosterskys/androidapp/viewentity/RamblerNewsViewEntity.java
+++ app/src/main/java/com/example/frosterskys/androidapp/viewentity/RamblerNewsViewEntity.java
@@ -1,12 +1,15 @@
 package com.example.frosterskys.androidapp.viewentity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.frosterskys.androidapp.R;
+import com.example.frosterskys.androidapp.RamblerNewsActivity;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -17,12 +20,19 @@ public class RamblerNewsViewEntity{
 
     private LinearLayout linearLayout;
 
-    public RamblerNewsViewEntity(Context context, String newsTitle, String imageRef) {
+    public RamblerNewsViewEntity(final Context context, String newsTitle, String imageRef) {
         linearLayout = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.rambler_news, null);
         ImageView imageView = (ImageView) linearLayout.getChildAt(0);
         Picasso.with(context).load(imageRef).into(imageView);
         TextView title = (TextView) linearLayout.getChildAt(1);
         title.setText(newsTitle);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, RamblerNewsActivity.class);
+                context.getApplicationContext().startActivity(intent);
+            }
+        });
     }
 
     public LinearLayout getLinearLayout(){
