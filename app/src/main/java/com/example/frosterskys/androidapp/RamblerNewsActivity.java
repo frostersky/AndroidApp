cd0c1567e9405faa8bbded660ebbe2b711eaf784
evbe0615 <evgenii.belobragin@netcracker.com>
2017-12-21 19:03:44 +0300

    dfsdfsfsdfsd

--- app/src/main/java/com/example/frosterskys/androidapp/RamblerNewsActivity.java
+++ app/src/main/java/com/example/frosterskys/androidapp/RamblerNewsActivity.java
@@ -1,10 +1,27 @@
 package com.example.frosterskys.androidapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
 
 /**
  * Created by evbe0615 on 21-Dec-17.
  */
 
 public class RamblerNewsActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rambler_page);
+
+        mWebView = (WebView) findViewById(R.id.ramblerNewsWebPage);
+        // включаем поддержку JavaScript
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        String url = getIntent().getStringExtra("newsRef");
+        mWebView.loadUrl(url);
+    }
 }
